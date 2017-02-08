5523f02e3acf49f2732acaa40c01b154374bb281
FreakishLancer <destfinis@gmail.com>
2017-02-08 18:39:43 -0500

    Proper proportions

--- js/script.js
+++ js/script.js
@@ -5,11 +5,9 @@ $(document).ready(() => {
     $("#strict-mode").on("click", () => {
         if (!isStrictMode) {
             $("#strict-mode span").text("On");
-            $("#strict-mode").css("left", "21.5%");
             isStrictMode = true;
         } else {
             $("#strict-mode span").text("Off");
-            $("#strict-mode").css("left", "20.5%");
             isStrictMode = false;
         }
     });
