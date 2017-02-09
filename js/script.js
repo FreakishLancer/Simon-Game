e8c2577c8bce6933d2d03f7b265045bec93f0d45
FreakishLancer <destfinis@gmail.com>
2017-02-08 19:34:44 -0500

    Added round number

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let isStrictMode = false;
+let isStrictMode = false,
+    powerOn = false;
 
 $(document).ready(() => {
 
@@ -12,10 +13,37 @@ $(document).ready(() => {
         }
     });
 
+    $("#power").on("click", () => {
+        if (!powerOn) {
+            $("#strict-mode").prop("disabled", true);
+            powerOn = true;
+        } else {
+            $("#strict-mode").prop("disabled", false);
+            powerOn = false;
+        }
+
+
+    });
+
 });
 
 // Pitch to Frequency Mappings
-// B4 494
-// G4 392
-// E4 330
-// C4 262
+// B3 247
+// G3 196
+// E3 165
+// C3 131
+
+/*
+
+c = new AudioContext();
+osc = c.createOscillator();
+gainer = c.createGain();
+gainer.gain.value = .15;
+osc.frequency.value = 330;
+osc.type = "sawtooth";
+osc.connect(gainer);
+osc.start(0);
+
+
+
+*/
